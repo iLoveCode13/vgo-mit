c4016ac5db84c7e75ebd0b598602f21896a19b6d
Joshua Rapoport <j.wish.2012@gmail.com>
2018-11-29 01:12:24 -0500

    Added "exec" boolean property.

--- scripts/members.js
+++ scripts/members.js
@@ -1,1 +1,23 @@
-var src = fetch("members.json").then(data => data.json());
+var vgo = {};
+var temp;
+
+/* Get `vgo` members list */
+fetch("members.json")
+    .then(response => response.json())
+    .then(function(json) {
+        vgo = json;
+    });
+
+/* Get `template` with id "memberCard" */
+temp = document.getElementById("memberCard");
+
+for (var member in vgo) {
+    var card = temp.content.clone();
+
+    card.getElementById("name").innerText = member.name;
+    card.getElementById("exec").hidden = member.exec;
+    card.getElementById("class").innerText = ((isNumber(member.class)) ? "Class of " : "") + member.class;
+    card.getElementById("plays").innerText = member.plays;
+
+    document.getElementById("content").appendChild(card);
+}
